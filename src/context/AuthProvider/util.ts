97012b69c2a468739cf8240196a84e4c7e0854a4
Helena Oliveira <helena.oliveira@hypeflame.com.br>
2022-07-22 09:27:48 -0300

    setItem and getItem user in the localStorage

--- src/context/AuthProvider/util.ts
+++ src/context/AuthProvider/util.ts
@@ -1,4 +1,21 @@
 import { Api } from "../../services/api";
+import { IUser } from "./types";
+
+//salvar o user no localStorage
+export function setUserLocalStorage(user: IUser | null) {
+  localStorage.setItem("u", JSON.stringify(user));
+}
+
+export function getUserLocalStorage() {
+  const json = localStorage.getItem("u");
+
+  if (!json) {
+    return null;
+  }
+
+  const user = JSON.parse(json);
+  return user ?? null;
+}
 
 export async function LoginResquest(email: string, password: string) {
   try {
