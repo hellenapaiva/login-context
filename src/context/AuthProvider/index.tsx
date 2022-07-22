06ba22e74647ae398322ff7493778256e67f8e65
Helena Oliveira <helena.oliveira@hypeflame.com.br>
2022-07-22 16:38:39 -0300

    store state inside localStorage

--- src/context/AuthProvider/index.tsx
+++ src/context/AuthProvider/index.tsx
@@ -1,6 +1,10 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { IAuthProvider, IContext, IUser } from "./types";
-import { LoginResquest } from "./util";
+import {
+  getUserLocalStorage,
+  LoginResquest,
+  setUserLocalStorage,
+} from "./util";
 
 export const AuthContext = createContext<IContext>({} as IContext);
 
@@ -8,15 +12,24 @@ export const AuthContext = createContext<IContext>({} as IContext);
 export const AuthProvider = ({ children }: IAuthProvider) => {
   const [user, setUser] = useState<IUser | null>();
 
+  useEffect(() => {
+    const user = getUserLocalStorage();
+    if (user) {
+      setUser(user);
+    }
+  }, []);
+
   async function authenticate(email: string, password: string) {
     const response = await LoginResquest(email, password);
 
     const payload = { token: response.token, email };
     setUser(payload);
+    setUserLocalStorage(payload);
   }
 
   function logout() {
     setUser(null);
+    setUserLocalStorage(null);
   }
 
   return (
